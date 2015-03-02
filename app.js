cde15fe3cdcad7e4d6912a8427fd90ac1e7a72d3
John Resig <jeresig@gmail.com>
2015-03-02 17:29:32 -0500

    Start to work on handling file uploads and un-zipping the zip files.

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var path = require("path");
 var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
+//var bodyParser = require("body-parser");
+var busboy = require("connect-busboy");
 
 var app = express();
 
@@ -14,8 +15,9 @@ app.set("view engine", "hbs");
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + "/public/favicon.ico"));
 app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(busboy());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
