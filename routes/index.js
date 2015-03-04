b127ff6d3a810de68efe76cbe9da641e78866da1
John Resig <jeresig@gmail.com>
2015-03-03 22:21:56 -0500

    Fix up changes to wrong files.

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-var unzip = require("unzip");
-
 /* GET home page. */
-router.get("/", function(req, res, next) {
-    req.pipe(req.busboy);
-
-    req.busboy.on("file", function(field, file, fileName) {
-        file
-            .pipe(unzip.Extract())
-            .on("entry", function(entry) {
-                var fileName = entry.path;
-                var type = entry.type; // Directory or File
-
-                if (true) {
-                    entry.pipe(fs.createWriteStream(fileName));
-                } else {
-                    entry.autodrain();
-                }
-            })
-            .on("close", function() {
-                // TODO: Start job
-                res.render("index", { title: "Express" });
-            });
-    });
+router.post("/new", function(req, res, next) {
+    res.render("index", { title: "Express" });
 });
 
 module.exports = router;
--- routes/upload.js
+++ routes/upload.js
@@ -6,9 +6,36 @@ var ME = require("matchengine")({
     password: process.env.ME_PASSWORD
 });
 
+var unzip = require("unzip");
+
 /* POST new upload */
-router.post("/new", function(req, res, next) {
-    res.render("index", { title: "Express" });
+router.get("/new", function(req, res, next) {
+    req.pipe(req.busboy);
+
+    var jobID = 1234;
+    var jobDir = "./";
+    var files = [];
+
+    req.busboy.on("file", function(field, file, fileName) {
+        file
+            .pipe(unzip.Extract())
+            .on("entry", function(entry) {
+                var fileName = entry.path;
+                var type = entry.type; // Directory or File
+
+                if (type === "File" && /\.jpe?g$/i.test(fileName)) {
+                    files.push(fileName);
+                    entry.pipe(fs.createWriteStream(fileName));
+                } else {
+                    entry.autodrain();
+                }
+            })
+            .on("close", function() {
+                // TODO: Start job
+                // Use files array
+                res.render("index", { title: "Express" });
+            });
+    });
 });
 
 module.exports = router;
