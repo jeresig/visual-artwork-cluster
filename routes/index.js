e220d29f8de125ef53cf671ca94aa29f8c44d94f
John Resig <jeresig@gmail.com>
2015-03-10 23:22:05 -0400

    Start to lay out the form and results for the home page.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
+
+var Job = mongoose.model("Job");
 
 /* GET home page. */
 router.post("/new", function(req, res, next) {
-    res.render("index", { title: "Express" });
+    Job.find().lean().exec(function(err, jobs) {
+        res.render("index", {
+            jobs: jobs
+        });
+    });
 });
 
 module.exports = router;
