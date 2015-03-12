8c8106f455d55769bc8d042d65579eefc4c43a08
John Resig <jeresig@gmail.com>
2015-03-11 22:36:30 -0400

    Start working on the job view.

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,22 @@ var mongoose = require("mongoose");
 var Job = mongoose.model("Job");
 
 /* GET home page. */
-router.post("/new", function(req, res, next) {
+router.post("/", function(req, res, next) {
     Job.find().lean().exec(function(err, jobs) {
         res.render("index", {
             jobs: jobs
         });
     });
+});
+
+router.post("/jobs/:jobName", function(req, res, next) {
+    Job.findById(req.params.jobName)
+        .populate("images")
+        .exec(function(err, job) {
+            res.render("job", {
+                job: job
+            });
+        });
 });
 
 module.exports = router;
