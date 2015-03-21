88daef6985aca662abba7e70946f103123c0b4cd
John Resig <jeresig@gmail.com>
2015-03-20 23:57:32 -0400

    Only show a link for completed jobs.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ var Job = mongoose.model("Job");
 /* GET home page. */
 router.get("/", function(req, res, next) {
     Job.find().lean().exec(function(err, jobs) {
+
+        jobs.forEach(function(job) {
+            job.completed = job.state === "completed";
+        });
+
         res.render("index", {
             title: "Visual Artwork Cluster",
             jobs: jobs
