38cdf31ba686492d0779fb4017840024fa98f722
John Resig <jeresig@gmail.com>
2015-03-27 23:48:20 -0400

    Handle marking image clusters as completed using Ajax.

---
+++ public/javascripts/site.js
@@ -0,0 +1,23 @@
+$(document).on("submit", "div.process form", function() {
+    var $form = $(this);
+    var $process = $form.parent();
+
+    // TODO: Show loading indicator
+    $form.hide();
+
+    $.ajax({
+        url: this.action,
+        method: "POST",
+        timeout: 5000,
+        success: function() {
+            $process
+                .append("<span class='processed'>(Already processed.)</span>");
+        },
+        error: function() {
+            // TODO: Show error message
+            $form.show();
+        }
+    });
+
+    return false;
+});
