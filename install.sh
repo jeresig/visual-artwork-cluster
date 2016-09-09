c39a557a6998fce8aeee23237de666556f711de4
John Resig <jeresig@gmail.com>
2016-09-09 11:43:46 -0400

    Fix crontab installation.

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ npm install
 
 # Install process-images script which will be used to periodically
 # move data through the process queue
-(crontab -l ; echo "* * * * * node `pwd`/process-images.js") | sort - | uniq -
+(crontab -l ; echo "* * * * * node `pwd`/process-images.js") | sort - | uniq - | crontab -
